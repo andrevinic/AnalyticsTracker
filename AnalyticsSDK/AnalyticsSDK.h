fd64df2183cd5b5903b0d0399c4783e14908cda2
André Nogueira <avinogueira@yahoo.com>
2019-01-18 23:47:06 -0200

    Initial Commit

---
+++ AnalyticsSDK/AnalyticsSDK.h
@@ -0,0 +1,19 @@
+//
+//  AnalyticsSDK.h
+//  AnalyticsSDK
+//
+//  Created by André Nogueira on 1/18/19.
+//  Copyright © 2019 André Nogueira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AnalyticsSDK.
+FOUNDATION_EXPORT double AnalyticsSDKVersionNumber;
+
+//! Project version string for AnalyticsSDK.
+FOUNDATION_EXPORT const unsigned char AnalyticsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnalyticsSDK/PublicHeader.h>
+
+
